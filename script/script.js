b29c14a9a83d76d8694811c9de982b8c4ec5b65b
MohamedAhmed <mohamed.essmattawfik@gmail.com>
2023-01-08 18:49:48 +0200

    first commit

---
+++ script/script.js
@@ -0,0 +1,14 @@
+// add class navbarDark on navbar scroll
+const header = document.querySelector('.navbar')
+console.log(header)
+header.classList.add('navbarDark')
+
+// collapse navbar after click on small devices
+const navLinks = document.querySelectorAll('.nav-item')
+const menuToggle = document.getElementById('navbarSupportedContent')
+
+navLinks.forEach((l) => {
+  l.addEventListener('click', () => {
+    new bootstrap.Collapse(menuToggle).toggle()
+  })
+})
